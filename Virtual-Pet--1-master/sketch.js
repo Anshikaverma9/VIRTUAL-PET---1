bbc3520d3875187085a42a42c7c76df828217fb4
Anshikaverma9 <74491364+Anshikaverma9@users.noreply.github.com>
2021-01-13 23:05:02 +0530

    Add files via upload

---
+++ Virtual-Pet--1-master/sketch.js
@@ -0,0 +1,22 @@
+//Create variables here
+var dog,dogImg;
+function preload()
+{
+	//load images here
+}
+
+function setup() {
+	createCanvas(500, 500);
+  
+}
+
+
+function draw() {  
+   background("green");
+  drawSprites();
+  //add styles here
+
+}
+
+
+
